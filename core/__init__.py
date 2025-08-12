3b35430c5e91facfb4ea65461c41ec16e4903449
Szyryngo <robert.szyryngo@gmail.com>
2025-08-12 13:14:38 +0200

    Update core/__init__.py with new APP_NAME and version

--- core/__init__.py
+++ core/__init__.py
@@ -1,2 +1,6 @@
 APP_NAME = "AI Network Sniffer"
 __version__ = "0.3.2"
+
+# Eksport najważniejszych klas/funkcji z core dla wygodnych importów
+from .ai_engine import AIEngine
+from .utils import PacketInfo, LogWriter, make_fake_packet
